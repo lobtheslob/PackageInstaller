92107857710082513b8ab58505f9f5165b5b1c8f
Tyler Simpson <Tyler.Simpson@visiblescm.com>
2018-10-05 06:24:54 -0600

    Finalizing Dependecy Resolver, used independent/dependent checks, instead of topological sort functionality with HashSet and Stack

--- PackageInstaller.UnitTests/PackageinstallerTest.cs
+++ PackageInstaller.UnitTests/PackageinstallerTest.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using NUnit.Framework;
 using PackageInstaller;
@@ -13,7 +16,10 @@ namespace PackageInstaller.UnitTests
     {
         private Packages.PackageInstaller _packageInstaller;
         private List<Package> _packages;
-        private string[] _dependancyList;
+        private string[] _dependencyList;
+        private string[] __dependencyInvalidList;
+        private string[] _correctlySortedList;
+        private string _correctlyStringOutput;
 
         [Test]
         public void AddPackages()
@@ -22,22 +28,22 @@ namespace PackageInstaller.UnitTests
             List<Package> _packagesTestList = new List<Package>()
             {
                 new Package("KittenService", "CamelCaser"),
-                new Package("CamelCaser", "")
+                new Package("CamelCaser","")
             };
 
             //instantiate string array
-            _dependancyList = new[]
-                        {
-                    "KittenService: CamelCaser",
-                    "CamelCaser: "
-                };
+            _dependencyList = new[]
+            {
+                "KittenService: CamelCaser",
+                "CamelCaser: "
+            };
 
             //instantiate Packcage class
             _packageInstaller = new Packages.PackageInstaller();
 
 
             //Add Packages
-            _packages = _packageInstaller.AddPackagesToList(_dependancyList);
+            _packages = _packageInstaller.AddPackagesToList(_dependencyList);
 
             Assert.That(_packages, Is.Not.Null);
             Assert.That(_packagesTestList, Is.Not.Null);
@@ -49,6 +55,71 @@ namespace PackageInstaller.UnitTests
 
         }
 
+        [Test]
+        public void PackagesAndTheirDependencies()
+        {
 
+            //instantiate string array
+            _dependencyList = new[]
+            {
+                "KittenService:",
+                "Leetmeme: Cyberportal",
+                "Cyberportal: Ice",
+                "CamelCaser: KittenService",
+                "Fraudstream: Leetmeme",
+                "Ice: "
+            };
+
+            _correctlySortedList = new[]
+            {
+                "KittenService",
+                "Ice",
+                "Cyberportal",
+                "Leetmeme",
+                "CamelCaser",
+                "Fraudstream"
+            };
+
+            _correctlyStringOutput = "KittenService, Ice, Cyberportal, Leetmeme, CamelCaser, Fraudstream";
+
+            //instantiate Packcage class
+            _packageInstaller = new Packages.PackageInstaller();
+
+            //Add Resolve Dependecies
+            var sortedOutput = _packageInstaller.OutputPackageInstaller(_dependencyList);
+            var result = String.Join(", ", sortedOutput.ToArray());
+
+
+            Assert.That(sortedOutput, Is.Not.Null);
+            Assert.That(sortedOutput.FirstOrDefault(), Is.EqualTo(_correctlySortedList[0]));
+            Assert.That(_correctlyStringOutput.Length, Is.EqualTo(result.Length));
+
+        }
+
+        [Test]
+        public void PackagesAndTheirCycles()
+        {
+
+            //instantiate string array
+            __dependencyInvalidList = new[]
+            {
+                "KittenService: ",
+                "Leetmeme: Cyberportal",
+                "Cyberportal: Ice",
+                "CamelCaser: KittenService",
+                "Fraudstream: ",
+                "Ice: Leetmeme"
+            };
+
+            //instantiate Packcage class
+            _packageInstaller = new Packages.PackageInstaller();
+
+            //Add Resolve Dependecies
+            var sortedOutput =_packageInstaller.OutputPackageInstaller(_dependencyList);
+            var result = String.Join(", ", sortedOutput.ToArray());
+
+            Assert.That(result, Is.EqualTo("error parsing packages: Invalid Input"));
+
+        }
     }
 }
